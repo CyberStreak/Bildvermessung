640b4d706efa6d4808a989c3c4ac0ea0ab541871
Milan Eric <milan.eric@students.fhnw.ch>
2022-12-23 15:01:26 +0100

    Json reader clean up

--- src/main/java/io/JsonReaderImage.java
+++ src/main/java/io/JsonReaderImage.java
@@ -1,22 +1,16 @@
 package io;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
 import java.util.Optional;
-
 import logic.ImageGenerator;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-//import javax.json.stream.JsonParser;
-//import javax.json.stream.JsonParser.Event;
-
 public class JsonReaderImage implements ImageDataReader {
-
-    JSONParser parser = new JSONParser();
 
     @Override
     public Optional<ImageGenerator> read(File file) throws FileNotFoundException {
@@ -24,12 +18,10 @@ public class JsonReaderImage implements ImageDataReader {
         JSONParser jsonparser = new JSONParser();
         FileReader reader = new FileReader(file.getAbsolutePath());
 
-        Object obj = null;
+        Object obj;
         try {
             obj = jsonparser.parse(reader);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (ParseException e) {
+        } catch (IOException | ParseException e) {
             throw new RuntimeException(e);
         }
         JSONObject imgJsonobj = (JSONObject)obj;
